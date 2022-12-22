208416ea04e28cdc6482dfa500d4b8ebf7824b5b
mspil88 <matthewspillane88@gmail.com>
2022-12-22 10:48:25 +0000

    bug fix - incorrect imports

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
-const HTMLWebPackPlugin = require("html-webpack-plugin");
-const {cleanWebpackPlugin} = require("clean-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
