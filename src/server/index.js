646146662fe9835e487d891527ad0f327b58e92a
mspil88 <matthewspillane88@gmail.com>
2022-12-28 09:09:56 +0000

    adding weather bit api

--- src/server/index.js
+++ src/server/index.js
@@ -22,11 +22,35 @@ const geoNamesUrl = (location, userName) => {
     return `http://api.geonames.org/searchJSON?name=${location}&maxRows=1&username=${userName}`
 }
 
+const weatherbitUrl = (longitude, latitude, weatherBitAPI) => {
+    return `http://api.weatherbit.io/v2.0/forecast/daily?key=${weatherBitAPI}&lat=${latitude}&lon=${longitude}`;
+}
+//http://api.weatherbit.io/v2.0/forecast/daily?key=1128eea57b1f47628f05f8eb4f91cbeb&lat=48.85341&lon=2.3488
 app.post("/userData", async (req, res) => {
     const location = req.body.location;
+    // const data = await axios.post(geoNamesUrl(location, process.env.GEONAMES_USER_NAME))
     const data = await axios.post(geoNamesUrl(location, process.env.GEONAMES_USER_NAME))
-    const {lng, lat, countryName} = data.data.geonames[0]
-    res.send({longitude: lng, latitude: lat, country: countryName})
+    .then(response => {
+        const {lng, lat, countryName} = response.data.geonames[0]
+        return [lng, lat, countryName]
+        
+    })
+    .then(data => {
+        //res.send(data)
+        console.log(data)
+
+        let rv = axios.post(weatherbitUrl(data[0], data[1], process.env.WEATHER_BIT_API_KEY))
+        return rv;
+    })
+    .then(res => {
+        console.log(res.data.data)
+    })
+
+    // res.send({longitude: data[0], latitude: data[1], country: data[2]})
+    // const {lng, lat, countryName} = data.data.geonames[0]
+    // res.send({longitude: lng, latitude: lat, country: countryName})
+    // return res.send({longitude: lng, latitude: lat, country: countryName})
+        
 })
 
 
